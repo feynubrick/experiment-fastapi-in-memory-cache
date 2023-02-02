e32d095bc9224e6b3f8738041944db159d846450
Seung-Yoon Oh <feynubrick@proton.me>
2023-02-02 23:18:15 +0900

    fastapi-cache를 사용해 count up 구현

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.inmemory import InMemoryBackend
+
 
 app = FastAPI()
 
@@ -31,3 +34,28 @@ async def increment_count_global_variable():
     next_val = count + 1 if count else 1
     update_value_from_in_memory("count", next_val)
     return next_val
+
+
+async def get_count():
+    return await FastAPICache.get_backend().get("count")
+
+
+async def update_count(value: int):
+    print("update_count called with: ", value)
+    return await FastAPICache.get_backend().set("count", value, 100)
+
+
+@app.put("/counts/increment/fastapi-cache")
+async def increment_count_in_memory_cache():
+    value = await get_count()
+    print(value)
+    next_value = value + 1 if value else 1
+    await update_count(next_value)
+    value = await get_count()
+    print(value)
+    return {"value": next_value}
+
+
+@app.on_event("startup")
+async def startup():
+    FastAPICache.init(InMemoryBackend(), prefix="fastapi-cache")
